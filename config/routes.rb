6c08763d09e5f78d2a21a6d10aab75cab7f59343
Sean Yu <seansu4you87@gmail.com>
2014-04-15 17:11:45 -0700

    Adding rspec config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/listings/:name' => 'listings#show'
+  post '/listings' => 'listings#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
